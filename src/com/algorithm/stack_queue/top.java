a6e20e8abb8f6751499ee9ca096e1238f72609ab
DESKTOP-QBKRSAC\HONG <gusdn90@gmail.com>
2020-06-25 18:08:40 +0900

    프로그래머스 top문제 구현 중

--- src/com/algorithm/stack_queue/top.java
+++ src/com/algorithm/stack_queue/top.java
@@ -4,9 +4,8 @@ import java.util.Queue;
 
 public class top {
     public static void main(String[] args) {
-        int[] sendTop = {6,9,5,7,4};
-
-        int[] result = solution(sendTop);
+        int[] sendTopArray = {6,9,5,7,4};
+        int[] result = solution(sendTopArray);
 
         // 결과 출력
         for(int s: result){
@@ -17,11 +16,18 @@ public class top {
 
     public static int[] solution(int[] heights) {
         int[] answer = {};
+        int heightTop = 0;
 
         //수신탑 큐
         Queue<Integer> reciveTop = new LinkedList<>();
 
+        for (int i=heights.length; i >=1; i--){
 
+            // 왼쪽으로 신호를 발사
+            if(heights[i-2] > heights[i-1]) {
+
+            }
+        }
 
 
         return answer;
