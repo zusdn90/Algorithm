3d926ba2a557794e39cfedc1478a663fa85ac7a5
zusdn90 <zusdn90@naver.com>
2020-06-25 23:04:48 +0900

    스택/큐 알고리즘 기능개발 구현 중

---
+++ src/com/algorithm/stack_queue/function_development.java
@@ -0,0 +1,52 @@
+package com.algorithm.stack_queue;
+
+import java.util.*;
+
+public class function_development {
+    public static void main(String[] args) {
+        int[] progresses = {93,30,55};
+        int[] speeds = {1,30,5};
+
+        int[] result = solution(progresses, speeds);
+
+        // 결과값 출력
+        for(int i=0; i<result.length; i++){
+            System.out.println(result[i]);
+        }
+    }
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        //progresses 배열 복사
+        int[] pro = progresses;
+
+        //배포수 저장
+        Queue<Integer> result = new LinkedList<>();
+        //배포할 수 있는 기준
+        int index =0;
+        //한번에 배포되는 수
+        int count=0;
+
+        while(index < pro.length) {
+            //하루 진도율 처리
+            for(int i=0; i<pro.length; i++) {
+                pro[i] += speeds[i];
+            }
+
+            if(pro[index] >= 100){  //index작업이 배포될 수 있으면
+                while(index < pro.length && pro[index] >= 100) { // 다른것도 같이 배포될 수 있는지 체크
+                    count++;
+                    index++;
+                }
+
+                result.offer(count);     //한번에 배포된 개수 저장
+                count = 0;
+            }
+        }
+
+        int[] answer = new int[result.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = result.poll();
+        }
+        return answer;
+    }
+}
--- src/com/algorithm/stack_queue/top.java
+++ src/com/algorithm/stack_queue/top.java
@@ -1,34 +1,34 @@
 package com.algorithm.stack_queue;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 
 public class top {
     public static void main(String[] args) {
         int[] sendTopArray = {6,9,5,7,4};
         int[] result = solution(sendTopArray);
-
         // 결과 출력
         for(int s: result){
             System.out.println(result[s]);
         }
-
     }
-
     public static int[] solution(int[] heights) {
-        int[] answer = {};
-        int heightTop = 0;
+        int[] answer = new int[heights.length];
+        //수신탑 스택
+        Stack<Integer> reciveTop = new Stack<Integer>();
 
-        //수신탑 큐
-        Queue<Integer> reciveTop = new LinkedList<>();
-
-        for (int i=heights.length; i >=1; i--){
-
-            // 왼쪽으로 신호를 발사
-            if(heights[i-2] > heights[i-1]) {
-
+        for (int i=heights.length-1; i >0; i--){
+            for (int j=i-1; j>=0; j--){
+                if(heights[j] > heights[i]){
+                    reciveTop.push(j+1);
+                    break;
+                }
+                if(j==0) reciveTop.push(0);
             }
         }
 
+        answer[0]=0;
+        for(int i=1; i < heights.length; i++){
+            answer[i] = reciveTop.pop();
+        }
 
         return answer;
     }
